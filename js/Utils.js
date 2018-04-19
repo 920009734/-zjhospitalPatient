146327024b87555325d97bb49f0612cc33d0db63
13256370616 <qwer1234>
2018-04-19 10:44:16 +0800

    个人信息、退出、新地址

--- js/Utils.js
+++ js/Utils.js
@@ -4,8 +4,9 @@ Utils = {
 	 */
 	getRoot : function() {
 		/*return "http://112.93.117.28:8089/zjhospital-app";*/
-		return "http://172.18.30.16:8080/zjhospital-app";
-		/*return "http://localhost:8080/zjhospital-app";*/
+		//return "http://172.18.30.16:8080/zjhospital-app";
+		return "http://219.137.32.146:8080/zjhospital-app";
+		//return "http://localhost:8088/zjhospital-app";
 		
 	},
 	/**
