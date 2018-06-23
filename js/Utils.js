e7ff889ab653b659f3502002b1598800e0595e3a
yy17756 <yuanye4068944>
2018-06-23 13:20:00 +0800

    预约

--- js/Utils.js
+++ js/Utils.js
@@ -45,6 +45,31 @@ Utils = {
 		return date.format(pattern);
 	},
 	/**
+	 * 把Date类型转为yyyy-MM-dd hh:mm:ss格式的字符串
+	 * 
+	 * @param l
+	 *            Date类型
+	 * 
+	 * @return 日期字符串
+	 */
+	formatDateStr : function(fmt,date) {  
+	  var o = {   
+	    "M+" : date.getMonth()+1,                 //月份   
+	    "d+" : date.getDate(),                    //日   
+	    "h+" : date.getHours(),                   //小时   
+	    "m+" : date.getMinutes(),                 //分   
+	    "s+" : date.getSeconds(),                 //秒   
+	    "q+" : Math.floor((date.getMonth()+3)/3), //季度   
+	    "S"  : date.getMilliseconds()             //毫秒   
+	  };   
+	  if(/(y+)/.test(fmt))   
+	    fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));   
+	  for(var k in o)   
+	    if(new RegExp("("+ k +")").test(fmt))   
+	  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+	  return fmt;   
+	},
+	/**
 	 * 把Date类型转为yyyy-MM-dd格式的字符串
 	 * 
 	 * @param l
