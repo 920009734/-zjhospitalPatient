126ea4be36e237392faf4edbf4630bc773bc2cb5
TP <TP@TP-PC>
2018-07-31 17:58:13 +0800

    动态数据加载

--- js/Utils.js
+++ js/Utils.js
@@ -305,7 +305,35 @@ Utils = {
 	    	}
 	    } );
 	    dtask.start();
-	}
+	},
+	
+	
+	/*1.用浏览器内部转换器实现html转码*/
+    htmlEncode:function (html){
+        var s = "";
+        if(html.length == 0) return "";
+        s = html.replace(/&/g,"&amp;");
+        s = s.replace(/</g,"&lt;");
+        s = s.replace(/>/g,"&gt;");
+        s = s.replace(/ /g,"&nbsp;");
+        s = s.replace(/\'/g,"&apos;");
+        s = s.replace(/\"/g,"&quot;");
+        return s; 
+    },
+    /*2.用浏览器内部转换器实现html解码*/
+    htmlDecode:function (text){
+        var s = "";
+        if(text.length == 0) return "";
+        s = text.replace(/&amp;/g,"&");
+        s = s.replace(/&lt;/g,"<");
+        s = s.replace(/&gt;/g,">");
+        s = s.replace(/&nbsp;/g," ");
+        s = s.replace(/&apos;/g,"\'");
+        s = s.replace(/&quot;/g,"\"");
+        return s;
+    }
+	
+	
 }
 
 /**
